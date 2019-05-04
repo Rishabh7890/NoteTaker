03c003f70eb34e0668c9d69cec9c12774f6e1f66
Rishabh Goel <Rishabh@rishabhs-mbp.lan>
2019-05-04 09:30:03 -0400

    added initial seeds to db

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+use u93evlx9bhysgnq6;
+
+INSERT INTO notes
+  (title, body)
+VALUES
+  ("Random Note", "I like javascript");
+
+INSERT INTO notes
+  (title, body)
+VALUES
+  ("Randomer Note 2", "I like sql too");

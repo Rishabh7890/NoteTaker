7bd9395bc91cbb13b6a7d899d577c8dddd470315
Rishabh Goel <Rishabh@rishabhs-mbp.lan>
2019-05-04 15:29:50 -0400

    cleaned up a bit more

--- public/assets/javascript/notes.js
+++ public/assets/javascript/notes.js
@@ -1,38 +1,36 @@
 // create function to show the notes in db 
 function showNotes() {
   $.ajax({
-    url: "api/notes",
-    method: "GET"
-  })
-  .then(function(data) {
-    console.log(data);
+      url: "api/notes",
+      method: "GET"
+    })
+    .then(function (data) {
+      console.log(data);
 
-    for (var i = 0; i < data.length; i++) {
-      // create reference to list and fill it with the data 
-      var notesList = $("#notes-list");
+      for (var i = 0; i < data.length; i++) {
+        // create reference to list and fill it with the data 
+        var notesList = $("#notes-list");
 
-      var listNote =  $("<li class='list-group-item'>");
+        var listNote = $("<li class='list-group-item'>");
 
-      // append listNote to the list
-      listNote.append(
-        $("<h5>").text("Note Number: " + (i + 1)),
-              $("<hr>"),
-              $("<h5>").text("Title: " + data[i].title),
-              $("<h6>").text(data[i].body),
-              $("<hr>"),
-              $("<i class='delete-note fas fa-trash'>")
-      );
+        // append listNote to the list
+        listNote.append(
+          $("<h5>").text("Title: " + data[i].title),
+          $("<h6>").text(data[i].body),
+          $("<hr>"),
+          $("<i class='delete-note fas fa-trash'>")
+        );
 
-      listNote.attr("note-data", data[i].id)
+        listNote.attr("note-data", data[i].id)
 
-      notesList.append(listNote);
-    }
-    
-  })
+        notesList.append(listNote);
+      }
+
+    })
 }
 
 // create an on click listner for submit button and add note to db and html 
-$("#submit").on("click", function(event) {
+$("#submit").on("click", function (event) {
   event.preventDefault();
 
   var newNote = {
@@ -43,14 +41,14 @@ $("#submit").on("click", function(event) {
   console.log(newNote);
 
   $.ajax({
-    url: "/api/notes",
-    method: "POST",
-    data: newNote
-  })
-  .then(function(data) {
-    $("#note-title").val("");
-    $("#note-body").val("")
-  })
+      url: "/api/notes",
+      method: "POST",
+      data: newNote
+    })
+    .then(function (data) {
+      $("#note-title").val("");
+      $("#note-body").val("")
+    })
   $("#note-title").val("");
   $("#note-body").val("");
 
@@ -59,22 +57,20 @@ $("#submit").on("click", function(event) {
 })
 
 // create delete button to remove note 
-$(document).on("click", ".delete-note", function(event) {
+$(document).on("click", ".delete-note", function (event) {
 
   var $noteID = $(this).parent().attr("note-data");
   var $noteIDInt = parseInt($noteID);
 
   $.ajax({
-    url: "/api/notes/" + $noteIDInt,
-    method: "DELETE"
-  })
-  .then(function() {
-    $("#notes-list").empty();
-    showNotes();
-  })
+      url: "/api/notes/" + $noteIDInt,
+      method: "DELETE"
+    })
+    .then(function () {
+      $("#notes-list").empty();
+      showNotes();
+    })
 })
 
 // run showNotes at end
 showNotes();
-
-
